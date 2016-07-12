44cf447a58fb7eb2465ff5bc85348e86c36edce1
Diego Dzienciol <diego@hinch.as>
2016-07-11 22:09:32 -0300

    Initial Commit

---
+++ betaTestHinchas/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  betaTestHinchas
+//
+//  Created by Diego Hinchas on 11/7/16.
+//  Copyright © 2016 Diego Hinchas. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ betaTestHinchas/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  betaTestHinchas
+//
+//  Created by Diego Hinchas on 11/7/16.
+//  Copyright © 2016 Diego Hinchas. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
